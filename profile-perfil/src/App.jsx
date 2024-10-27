a5fd46ed8e110ad534cc4e242fd1f39f28623dea
PedroAugusto2305 <p.augusto_23@outlook.com>
2024-10-27 20:17:46 -0300

    Profile Page

---
+++ profile-perfil/src/App.jsx
@@ -0,0 +1,18 @@
+import Profile from "./components/profile";
+
+export default function App() {
+    return (
+        <div className="app">
+            <Profile
+                avatar="https://avatars.githubusercontent.com/u/85378029?v=4"
+                nome="Avatar Pedro"
+                bio="Software Engineer"
+                email="pedro@email.com"
+                phone="+5512123456"
+                githubUrl="github.com"
+                linkedinUrl="linkedin.com"
+                twitterUrl="twitter.com"
+            />
+        </div>
+    )
+}
